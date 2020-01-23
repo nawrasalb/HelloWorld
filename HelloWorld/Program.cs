4548b04f807e101343704a9990d88126b427ce76
Nawras Albarwani <nawras@mail.usf.edu>
2020-01-23 14:08:57 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display hello world!
+            Console.WriteLine(" Press any key to continue...");
+            Console.Write(" Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
